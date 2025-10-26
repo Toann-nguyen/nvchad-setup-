51d7cc4c54bea1f9aedb2d7e00f326fdec046a30
Toan Nguyen <nguyenminhtoan2712py@gmail.com>
2025-10-26 17:17:10 +0700

    custom F5 F6 compile and run cpp

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,164 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
+-- Basic mappings
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Enhanced clipboard operations
+map({ "n", "v" }, "<C-c>", '"+y', { desc = "Copy to system clipboard" })
+map("n", "<C-c>", '"+yy', { desc = "Copy line to system clipboard" })
+map({ "n", "v" }, "<C-v>", '"+p', { desc = "Paste from system clipboard" })
+map("i", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard (insert)" })
+map("n", "<leader>ca", "ggVG\"+y", { desc = "Copy entire buffer to clipboard" })
+
+-- Competitive programming shortcuts
+map("n", "<leader>cc", function()
+  if SetCPTemplate then
+    SetCPTemplate()
+  else
+    vim.notify("SetCPTemplate function not loaded yet", vim.log.levels.WARN)
+  end
+end, { desc = "Insert CP template" })
+
+-- F5: Run tests with competitest
+map("n", "<F5>", function()
+  vim.cmd("w")
+  vim.cmd("CompetiTest run")
+end, { desc = "Run tests with competitest" })
+
+-- F6: Quick compile and run in split terminal (NEW)
+map("n", "<F6>", function()
+  vim.cmd("w")
+  local filename = vim.fn.expand("%:r")
+  local ext = vim.fn.expand("%:e")
+  
+  if ext == "cpp" then
+    -- Close existing terminal buffers
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.bo[buf].buftype == "terminal" then
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end
+    
+    -- Compile
+    local compile_cmd = string.format("g++ -std=c++17 -O2 -Wall -Wextra -o %s %s", filename, vim.fn.expand("%"))
+    vim.notify("Compiling...", vim.log.levels.INFO)
+    local compile_output = vim.fn.system(compile_cmd)
+    
+    if vim.v.shell_error == 0 then
+      vim.notify("Compilation successful! Running...", vim.log.levels.INFO)
+      -- Open horizontal split terminal and run
+      vim.cmd("split | terminal ./" .. filename)
+      vim.cmd("resize 15")
+      -- Enter insert mode in terminal
+      vim.cmd("startinsert")
+    else
+      vim.notify("Compilation failed!\n" .. compile_output, vim.log.levels.ERROR)
+    end
+  else
+    vim.notify("Not a C++ file!", vim.log.levels.WARN)
+  end
+end, { desc = "Compile and run C++ (F6)" })
+
+-- F9: Compile and run in new split (alternative method)
+map("n", "<F9>", function()
+  vim.cmd("w")
+  local filename = vim.fn.expand("%:r")
+  local ext = vim.fn.expand("%:e")
+  
+  if ext == "cpp" then
+    vim.cmd("!clear")
+    local compile_cmd = string.format("g++ -std=c++17 -O2 -Wall -Wextra -o %s %s", filename, vim.fn.expand("%"))
+    vim.cmd("!" .. compile_cmd)
+    
+    if vim.v.shell_error == 0 then
+      vim.cmd("split | terminal ./" .. filename)
+    else
+      vim.notify("Compilation failed!", vim.log.levels.ERROR)
+    end
+  else
+    vim.notify("Not a C++ file!", vim.log.levels.WARN)
+  end
+end, { desc = "Compile and run C++ (F9)" })
+
+-- Quick save and basic shortcuts
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "Save file" })
+map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
+map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+map("n", "<leader>Q", "<cmd>qa!<cr>", { desc = "Force quit all" })
+
+-- Buffer navigation (inspired by craftzdog)
+map("n", "<leader>bn", "<cmd>enew<cr>", { desc = "New buffer" })
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete buffer" })
+
+-- Enhanced navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "Switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "Switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Switch window up" })
+
+-- Window management (inspired by craftzdog)
+map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+-- Resize windows with arrows
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Move lines up/down (inspired by craftzdog)
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move selection up" })
+
+-- Better indenting
+map("v", "<", "<gv", { desc = "Indent left" })
+map("v", ">", ">gv", { desc = "Indent right" })
+
+-- Clear search highlight
+map("n", "<leader>nh", "<cmd>nohl<cr>", { desc = "Clear search highlights" })
+
+-- Increment/decrement numbers
+map("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+map("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+-- Format shortcuts
+map("n", "<leader>fm", function()
+  local conform = require("conform")
+  if conform then
+    conform.format({ lsp_fallback = true })
+  else
+    vim.lsp.buf.format()
+  end
+end, { desc = "Format file" })
+
+-- Diagnostics
+map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Floating diagnostic" })
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+map("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", { desc = "List diagnostics" })
+
+-- LSP shortcuts (inspired by craftzdog)
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+map("n", "gr", vim.lsp.buf.references, { desc = "Show references" })
+map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+
+-- Terminal mode mappings
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Terminal: Switch window left" })
+map("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Terminal: Switch window down" })
+map("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Terminal: Switch window up" })
+map("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Terminal: Switch window right" })
